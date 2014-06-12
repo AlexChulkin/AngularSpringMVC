8ce4a2541ac828ebc574eb82848d9456c6a3d197
vahe.harutyunyan <vahe.harutyunyan@hotmail.com>
2014-06-12 12:04:34 +0300

    Add DBCP as connection pool in hibernate.

--- src/main/java/com/luxoft/snp/config/PersistenceJPAConfig.java
+++ src/main/java/com/luxoft/snp/config/PersistenceJPAConfig.java
@@ -1,9 +1,9 @@
 package com.luxoft.snp.config;
 
+import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -35,13 +35,21 @@ public class PersistenceJPAConfig {
         return em;
     }
 
+
+
     @Bean
     public DataSource dataSource(){
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        BasicDataSource dataSource = new BasicDataSource ();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/testspring");
         dataSource.setUsername("root");
         dataSource.setPassword("");
+
+        dataSource.setMaxActive(10);
+        dataSource.setMaxWait(100);
+        dataSource.setInitialSize(3);
+
+
         return dataSource;
     }
 
