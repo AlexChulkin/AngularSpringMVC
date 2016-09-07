e22d4367488c7b6418fbb89ba3f5e45c142c0519
alex.chulkin@gmail.com <$x90u7b5>
2016-09-08 00:49:42 +0300

    Implemented the Servlet 3 Code-Based Configuration. Totall removed the xml config (web.xml)

---
+++ src/main/java/com/somecode/config/PacketAppAdminInitializer.java
@@ -0,0 +1,19 @@
+package com.somecode.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class PacketAppAdminInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext container) throws ServletException {
+        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+        appContext.setConfigLocation("com.somecode.config.Config");
+        ServletRegistration.Dynamic dispatcher = container.addServlet("appServlet", new DispatcherServlet(appContext));
+        dispatcher.addMapping("/");
+    }
+}
