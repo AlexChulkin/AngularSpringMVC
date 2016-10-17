a8f63ca3886bc78cd17c1f5db12d7054ae144b44
alex.chulkin@gmail.com <$x90u7b5>
2016-10-17 15:55:21 +0300

    directive test done

--- src/main/webapp/resources/tests/directivesTests.js
+++ src/main/webapp/resources/tests/directivesTests.js
@@ -1,46 +1,52 @@
 /**
  * Created by alexc_000 on 2016-10-17.
  */
-xdescribe("Directive Tests", function () {
+describe("Directive Tests", function () {
     var $rootScope,
-        $controller,
-        compileService,
         mockExchangeService,
         mockScope,
-        controller,
         el,
-    // $body = document.getElementById('body'),
-        simpleHtml = '<input id="newComptLabel" class="text" name="newLabelName" ng-model="data.newLabel"' + +'ng-trim="true" ng-maxlength="70" blacklist="blacklist" required/>';
+        $body = angular.element('<div>'),
+        simpleHtml = '<input id="newComptLabel" class="text" name="newLabelName" ng-model="data.newLabel"' +
+            'ng-trim="true" ng-maxlength="70" blacklist="blacklist" required/>';
+    
     beforeEach(module("packetAdminApp"));
-    beforeEach(inject(function ($injector, $compile, $controller, exchangeService) {
-        $rootScope = $injector.get($rootScope);
-        // $controller = $injector.get($controller);
-        compileService = $injector.get($compile);
+
+    beforeEach(inject(function ($injector, $compile, exchangeService) {
+        $rootScope = $injector.get('$rootScope');
         mockScope = $rootScope.$new();
         el = $compile(angular.element(simpleHtml))(mockScope);
         mockExchangeService = {
             getSelectedComptLabels: function () {
             }
         };
-        spyOn(mockExchangeService, 'getSelectedComptLabels').and.returnValue({'a': true, 'b': true});
-        controller = $controller("comptsPanelCtrl", {
-            $scope: mockScope,
-            exchangeService: mockExchangeService
-        });
+        exchangeService.setSelectedComptLabels({'ROMA LOCUTA CAUSA FINITA': true, 'DURA LEX SED LEX': true});
     }));
+
     beforeEach(function () {
-        // $body.append(el);
-        // el =  compileService(simpleHtml)(mockScope);
+        $body.append(el);
         $rootScope.$digest();
     });
-    it('blacklist directive test', function () {
-        el.val('a');
-        el.trigger('input');
-        mockScope.digest();
+
+    it('blacklist directive positive test spaces', function () {
+        el.val('Roma    locuta causa       finita');
+        el.triggerHandler('input');
         expect(el.hasClass('ng-invalid')).toBeTruthy();
     });
 
+    it('blacklist directive positive test letters case', function () {
+        el.val('Roma LOCUTA causa fINITA');
+        el.triggerHandler('input');
+        expect(el.hasClass('ng-invalid')).toBeTruthy();
+    });
+
+    it('blacklist directive negative test', function () {
+        el.val('Lorem ipsum');
+        el.triggerHandler('input');
+        expect(el.hasClass('ng-invalid')).toBeFalsy();
+    });
+
     afterEach(function () {
-        // $body.empty();
+        $body.empty();
     });
 });
